6136dd94e4cbbba0553bf5d52900afedfdb099c4
Andrew Weisman <andrew.weisman@nih.gov>
2023-12-07 02:26:04 -0500

    Address known issue of page running twice due to st_javascript by minimizing that effect by ensuring the page loads as quickly as possible by adding a load data button instead of just loading the data automatically on page load. Probably a good best practice to follow anyway: Make the pages load as quickly as possible, hiding potentially slow operations behind buttons

--- pages/multiaxial_gating.py
+++ pages/multiaxial_gating.py
@@ -50,7 +50,9 @@ def update_dependencies_of_button_for_adding_phenotype_to_new_dataset():
 
     # Get the current values of the two dataframe editors
     df_current_phenotype = st.session_state['mg__de_current_phenotype'].reconstruct_edited_dataframe()
-    df_phenotype_assignments = st.session_state['mg__de_phenotype_assignments'].reconstruct_edited_dataframe().set_index('Phenotype')
+    df_phenotype_assignments = st.session_state['mg__de_phenotype_assignments'].reconstruct_edited_dataframe()
+    if 'Phenotype' in df_phenotype_assignments.columns:
+        df_phenotype_assignments = df_phenotype_assignments.set_index('Phenotype')
 
     # Populate a dictionary of the column filteres for the current phenotype
     curr_phenotype_dict = dict()
@@ -147,8 +149,8 @@ def main():
     input_datafilename = 'measurementsthymus-exported.csv'
     coord_units_in_microns = 1
 
-    # Load the data only once, otherwise keep it in memory
-    if 'mg__df' not in st.session_state:
+    # Load the data
+    if st.button('Load data'):
         st.session_state['mg__df'] = load_data(os.path.join(input_directory, input_datafilename), coord_units_in_microns, dataset_formats.extract_datafile_metadata(os.path.join(input_directory, input_datafilename))[4])
         unique_images = st.session_state['mg__df']['Slide ID'].unique()
         st.session_state['mg__unique_images_short'] = [x.split('-imagenum_')[1] for x in unique_images]
@@ -156,132 +158,141 @@ def main():
         st.session_state['mg__all_numeric_columns'] = st.session_state['mg__df'].select_dtypes(include='number').columns
         phenotype_columns = [column for column in st.session_state['mg__df'].columns if column.startswith('Phenotype ')]
         st.session_state['mg__df'] = st.session_state['mg__df'].rename(columns=dict(zip(phenotype_columns, [column.replace('Phenotype ', 'Phenotype_orig ') for column in phenotype_columns])))
-    df = st.session_state['mg__df']
-    unique_images_short = st.session_state['mg__unique_images_short']
-    unique_image_dict = st.session_state['mg__unique_image_dict']
+    
+    # Warn the user that they need to load the data at least once
+    if 'mg__df' not in st.session_state:
+        st.warning('You must click the "Load data" button above before you can use this app!')
 
-    # Define the main columns
-    main_columns = st.columns(3, gap='medium')
+    # If the data have been loaded...
+    else:
+    
+        # Load the data and some resulting processed data
+        df = st.session_state['mg__df']
+        unique_images_short = st.session_state['mg__unique_images_short']
+        unique_image_dict = st.session_state['mg__unique_image_dict']
 
-    # Data column filter
-    with main_columns[0]:
+        # Define the main columns
+        main_columns = st.columns(3, gap='medium')
 
-        # Column header
-        st.header(':one: Column filter')
+        # Data column filter
+        with main_columns[0]:
 
-        # Have a dropdown for the column on which to perform a kernel density estimate
-        st.selectbox(label='Column for filtering:', options=update_column_options(), key='mg__column_for_filtering', on_change=update_dependencies_of_column_for_filtering)
-        column_for_filtering = st.session_state['mg__column_for_filtering']
+            # Column header
+            st.header(':one: Column filter')
 
-        # Output information on the column range
-        if 'mg__curr_column_range' not in st.session_state:
-            update_dependencies_of_column_for_filtering()
-        column_range = st.session_state['mg__curr_column_range']
-        st.write('Column range: {}'.format(column_range))
+            # Have a dropdown for the column on which to perform a kernel density estimate
+            st.selectbox(label='Column for filtering:', options=update_column_options(), key='mg__column_for_filtering', on_change=update_dependencies_of_column_for_filtering)
+            column_for_filtering = st.session_state['mg__column_for_filtering']
 
-        # Determine the x-y data to plot for the selected column, calculating the KDE for each column only once ever
-        if column_for_filtering not in list(st.session_state['mg__dfs_to_plot']):
-            line2d = sns.kdeplot(df, x=column_for_filtering).get_lines()[0]
-            curr_df = pd.DataFrame({'Value': line2d.get_xdata(), 'Density': line2d.get_ydata()})
-            st.session_state['mg__dfs_to_plot'][column_for_filtering] = curr_df[(curr_df['Value'] >= column_range[0]) & (curr_df['Value'] <= column_range[1])]  # needed because the KDE can extend outside the possible value range
-        df_to_plot_full = st.session_state['mg__dfs_to_plot'][column_for_filtering]
+            # Output information on the column range
+            if 'mg__curr_column_range' not in st.session_state:
+                update_dependencies_of_column_for_filtering()
+            column_range = st.session_state['mg__curr_column_range']
+            st.write('Column range: {}'.format(column_range))
 
-        # Write some text boxes for the desired ranges for the current analysis column. Could make this stronger by enforcing the min_value and max_value parameters in each widget to correspond to the other so that the chosen max is never less than the chosen min, but initial attempts at this shows strange behavior and isn't worth debugging for now
-        # Implement range slider here!
-        st.slider(label='Minimum value:', min_value=column_range[0], max_value=column_range[1], key='mg__selected_min_val')
-        st.slider(label='Maximum value:', min_value=column_range[0], max_value=column_range[1], key='mg__selected_max_val')
-        selected_min_val = st.session_state['mg__selected_min_val']
-        selected_max_val = st.session_state['mg__selected_max_val']
+            # Determine the x-y data to plot for the selected column, calculating the KDE for each column only once ever
+            if column_for_filtering not in list(st.session_state['mg__dfs_to_plot']):
+                line2d = sns.kdeplot(df, x=column_for_filtering).get_lines()[0]
+                curr_df = pd.DataFrame({'Value': line2d.get_xdata(), 'Density': line2d.get_ydata()})
+                st.session_state['mg__dfs_to_plot'][column_for_filtering] = curr_df[(curr_df['Value'] >= column_range[0]) & (curr_df['Value'] <= column_range[1])]  # needed because the KDE can extend outside the possible value range
+            df_to_plot_full = st.session_state['mg__dfs_to_plot'][column_for_filtering]
 
-        # Create a view of the full dataframe that is the selected subset
-        df_to_plot_selected = df_to_plot_full[(df_to_plot_full['Value'] >= selected_min_val) & (df_to_plot_full['Value'] <= selected_max_val)]
+            # Write some text boxes for the desired ranges for the current analysis column. Could make this stronger by enforcing the min_value and max_value parameters in each widget to correspond to the other so that the chosen max is never less than the chosen min, but initial attempts at this shows strange behavior and isn't worth debugging for now
+            # Implement range slider here!
+            st.slider(label='Minimum value:', min_value=column_range[0], max_value=column_range[1], key='mg__selected_min_val')
+            st.slider(label='Maximum value:', min_value=column_range[0], max_value=column_range[1], key='mg__selected_max_val')
+            selected_min_val = st.session_state['mg__selected_min_val']
+            selected_max_val = st.session_state['mg__selected_max_val']
 
-        # Plot the Plotly figure in Streamlit
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=df_to_plot_full['Value'], y=df_to_plot_full['Density'], fill='tozeroy', mode='none', fillcolor='yellow', name='Full dataset', hovertemplate=' '))
-        fig.add_trace(go.Scatter(x=df_to_plot_selected['Value'], y=df_to_plot_selected['Density'], fill='tozeroy', mode='none', fillcolor='red', name='Selection', hoverinfo='skip'))
-        fig.update_layout(hovermode='x unified', xaxis_title='Column value', yaxis_title='Density')
-        st.plotly_chart(fig)
+            # Create a view of the full dataframe that is the selected subset
+            df_to_plot_selected = df_to_plot_full[(df_to_plot_full['Value'] >= selected_min_val) & (df_to_plot_full['Value'] <= selected_max_val)]
 
-        # Add the current column filter to the current phenotype assignment
-        st.button(':star2: Add column filter to current phenotype :star2:', use_container_width=True, on_click=update_dependencies_of_button_for_adding_column_filter_to_current_phenotype, args=(column_for_filtering, selected_min_val, selected_max_val))
+            # Plot the Plotly figure in Streamlit
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=df_to_plot_full['Value'], y=df_to_plot_full['Density'], fill='tozeroy', mode='none', fillcolor='yellow', name='Full dataset', hovertemplate=' '))
+            fig.add_trace(go.Scatter(x=df_to_plot_selected['Value'], y=df_to_plot_selected['Density'], fill='tozeroy', mode='none', fillcolor='red', name='Selection', hoverinfo='skip'))
+            fig.update_layout(hovermode='x unified', xaxis_title='Column value', yaxis_title='Density')
+            st.plotly_chart(fig)
 
-    # Current phenotype and phenotype assignments
-    with main_columns[1]:
+            # Add the current column filter to the current phenotype assignment
+            st.button(':star2: Add column filter to current phenotype :star2:', use_container_width=True, on_click=update_dependencies_of_button_for_adding_column_filter_to_current_phenotype, args=(column_for_filtering, selected_min_val, selected_max_val))
 
-        # Column header
-        st.header(':two: Current phenotype', help='Note you can refine values in the following table by editing them directly or even deleting (or adding) whole rows.')
+        # Current phenotype and phenotype assignments
+        with main_columns[1]:
 
-        # Output the dataframe holding the phenotype that's currently being built
-        st.session_state['mg__de_current_phenotype'].process_data_editor(current_page_id=curr_url, previous_page_key='previous_url')
+            # Column header
+            st.header(':two: Current phenotype', help='Note you can refine values in the following table by editing them directly or even deleting (or adding) whole rows.')
 
-        # Choose a phenotype name
-        st.text_input(label='Phenotype name:', key='mg__current_phenotype_name')
+            # Output the dataframe holding the phenotype that's currently being built
+            st.session_state['mg__de_current_phenotype'].process_data_editor(current_page_id=curr_url, previous_page_key='previous_url')
 
-        # Add the current phenotype to the phenotype assignments table
-        st.button(label=':star2: Add phenotype to assignments table :star2:', use_container_width=True, on_click=update_dependencies_of_button_for_adding_phenotype_to_new_dataset)
+            # Choose a phenotype name
+            st.text_input(label='Phenotype name:', key='mg__current_phenotype_name')
 
-        # Column header
-        st.header(':three: Phenotype assignments', help='Note you can refine values in the following table by editing them directly or even deleting whole rows.')
+            # Add the current phenotype to the phenotype assignments table
+            st.button(label=':star2: Add phenotype to assignments table :star2:', use_container_width=True, on_click=update_dependencies_of_button_for_adding_phenotype_to_new_dataset)
 
-        # Output the dataframe holding the specifications for all phenotypes
-        st.session_state['mg__de_phenotype_assignments'].process_data_editor(current_page_id=curr_url, previous_page_key='previous_url')
+            # Column header
+            st.header(':three: Phenotype assignments', help='Note you can refine values in the following table by editing them directly or even deleting whole rows.')
 
-        # Generate the new dataset
-        st.button(label=':star2: Generate the new dataset from the phenotype assignments :star2:', use_container_width=True, on_click=add_new_phenotypes_to_main_df, args=(df,))
+            # Output the dataframe holding the specifications for all phenotypes
+            st.session_state['mg__de_phenotype_assignments'].process_data_editor(current_page_id=curr_url, previous_page_key='previous_url')
 
-    # New dataset
-    with main_columns[2]:
+            # Generate the new dataset
+            st.button(label=':star2: Generate the new dataset from the phenotype assignments :star2:', use_container_width=True, on_click=add_new_phenotypes_to_main_df, args=(df,))
 
-        # Column header
-        st.header(':four: New dataset')
+        # New dataset
+        with main_columns[2]:
 
-        # Print out a sample of the main dataframe
-        st.write('Augmented dataset sample:')
-        st.dataframe(st.session_state['mg__df'].sample(5))
+            # Column header
+            st.header(':four: New dataset')
 
-        # Get a list of all new phenotypes
-        new_phenotypes = [column for column in st.session_state['mg__df'].columns if column.startswith('Phenotype ')]
+            # Print out a sample of the main dataframe
+            st.write('Augmented dataset sample:')
+            st.dataframe(st.session_state['mg__df'].head(5))
 
-        # If at least one phenotype has been assigned...
-        if len(new_phenotypes) > 0:
+            # Get a list of all new phenotypes
+            new_phenotypes = [column for column in st.session_state['mg__df'].columns if column.startswith('Phenotype ')]
 
-            # Initialize the plot of an optional cell scatter plot to the first image in the dataset
-            if 'mg__image_to_plot' not in st.session_state:
-                st.session_state['mg__image_to_plot'] = unique_images_short[0]
-            if 'mg__phenotype_to_plot' not in st.session_state:
-                st.session_state['mg__phenotype_to_plot'] = new_phenotypes[0]
+            # If at least one phenotype has been assigned...
+            if len(new_phenotypes) > 0:
 
-            # Generate widgets for the plotting parameters
-            st.selectbox(label='Image to plot:', options=unique_images_short, key='mg__image_to_plot')
-            st.selectbox(label='Phenotype to plot:', options=new_phenotypes, key='mg__phenotype_to_plot')
+                # Initialize the plot of an optional cell scatter plot to the first image in the dataset
+                if 'mg__image_to_plot' not in st.session_state:
+                    st.session_state['mg__image_to_plot'] = unique_images_short[0]
+                if 'mg__phenotype_to_plot' not in st.session_state:
+                    st.session_state['mg__phenotype_to_plot'] = new_phenotypes[0]
 
-            # If the button is pressed
-            if st.button('Plot the selected phenotype in the selected image'):
-                df_for_scatterplot = df.loc[df['Slide ID'] == unique_image_dict[st.session_state['mg__image_to_plot']], ['Cell X Position', 'Cell Y Position', st.session_state['mg__phenotype_to_plot']]]
-                fig = px.scatter(data_frame=df_for_scatterplot, x='Cell X Position', y='Cell Y Position', color=st.session_state['mg__phenotype_to_plot'])
-                fig.update_xaxes(scaleanchor='y')
-                st.plotly_chart(fig)
+                # Generate widgets for the plotting parameters
+                st.selectbox(label='Image to plot:', options=unique_images_short, key='mg__image_to_plot')
+                st.selectbox(label='Phenotype to plot:', options=new_phenotypes, key='mg__phenotype_to_plot')
 
-            # Optionally run some checks
-            if st.toggle(label='Do checks'):
+                # If the button is pressed
+                if st.button('Plot the selected phenotype in the selected image'):
+                    df_for_scatterplot = df.loc[df['Slide ID'] == unique_image_dict[st.session_state['mg__image_to_plot']], ['Cell X Position', 'Cell Y Position', st.session_state['mg__phenotype_to_plot']]]
+                    fig = px.scatter(data_frame=df_for_scatterplot, x='Cell X Position', y='Cell Y Position', color=st.session_state['mg__phenotype_to_plot'])
+                    fig.update_xaxes(scaleanchor='y')
+                    st.plotly_chart(fig)
 
-                # Write out the detected cutoffs to use for basic validation
-                phenotypes_orig = ['Phenotype_orig MHCII', 'Phenotype_orig Ly51', 'Phenotype_orig EpCAM']
-                intensities = ['MHC II (CH3 Fluor Cy3): Membrane: Mean', 'Ly51 (CH4 Fluor Cy5): Membrane: Mean', 'EpCAM (CH2 Fluor GFP): Membrane: Mean']
-                for ipheno in range(len(phenotypes_orig)):
-                    tmp = st.session_state['mg__df'][phenotypes_orig[ipheno]]
-                    tmp.index = st.session_state['mg__df'][intensities[ipheno]]
-                    tmp = tmp.sort_index()
-                    st.write('Intensity cutoff for {}: {}'.format(phenotypes_orig[ipheno], tmp[tmp == '+'].index[0]))
+                # Optionally run some checks
+                if st.toggle(label='Do checks'):
 
-                # Write the numbers of positive markers to compare the new phenotypes with the original phenotypes
-                for phenotype_orig in phenotypes_orig:
-                    st.write(st.session_state['mg__df'][phenotype_orig].value_counts())
-                    st.write(st.session_state['mg__df'][phenotype_orig.replace('_orig', '').replace('MHCII', 'MHC II') + ' new'].value_counts())
+                    # Write out the detected cutoffs to use for basic validation
+                    phenotypes_orig = ['Phenotype_orig MHCII', 'Phenotype_orig Ly51', 'Phenotype_orig EpCAM']
+                    intensities = ['MHC II (CH3 Fluor Cy3): Membrane: Mean', 'Ly51 (CH4 Fluor Cy5): Membrane: Mean', 'EpCAM (CH2 Fluor GFP): Membrane: Mean']
+                    for ipheno in range(len(phenotypes_orig)):
+                        tmp = st.session_state['mg__df'][phenotypes_orig[ipheno]]
+                        tmp.index = st.session_state['mg__df'][intensities[ipheno]]
+                        tmp = tmp.sort_index()
+                        st.write('Intensity cutoff for {}: {}'.format(phenotypes_orig[ipheno], tmp[tmp == '+'].index[0]))
 
-    # Save the URL of the current page before we potentially leave the page. This must be present on every page
-    st.session_state['previous_url'] = curr_url
+                    # Write the numbers of positive markers to compare the new phenotypes with the original phenotypes
+                    for phenotype_orig in phenotypes_orig:
+                        st.write(st.session_state['mg__df'][phenotype_orig].value_counts())
+                        st.write(st.session_state['mg__df'][phenotype_orig.replace('_orig', '').replace('MHCII', 'MHC II') + ' new'].value_counts())
+
+        # Save the URL of the current page before we potentially leave the page. This must be present on every page
+        st.session_state['previous_url'] = curr_url
 
 if __name__ == '__main__':
     main()
