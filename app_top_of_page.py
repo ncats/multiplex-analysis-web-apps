5247c2536020f1bb290855cb984deaaf5738a023
Dante Smith <djsmith@bu.edu>
2023-12-26 22:10:52 -0600

    Updating TOP
    Updating what goes into the top of the page. The order of each step is now a measured step of processes

--- app_top_of_page.py
+++ app_top_of_page.py
@@ -1,10 +1,12 @@
 import numpy as np
 import platform_io
 import subprocess
+import streamlit as st
+from st_pages import show_pages_from_config, add_indentation
 from streamlit_javascript import st_javascript
 from streamlit_extras.app_logo import add_logo
 
-def top_of_page_reqs(session_state):
+def top_of_page_reqs(session_state, newtab_flag = False):
     '''
     Top of the page requirements. These are the commands
     that are run at the top of the page that help maintain 
@@ -14,11 +16,25 @@ def top_of_page_reqs(session_state):
     # for key, val in session_state.items():
     #     if (not key.endswith('__do_not_persist')) and (not key.startswith('FormSubmitter:')):
     #         session_state[key] = val
+
     # Add Logo
-    # add_logo('app_images/mawa_logo-width315.png', height=150)
-    
-    # Get the URL of the current page, per https://discuss.streamlit.io/t/what-is-the-current-page-in-use-multipage-app/41898, which we've used before but keeping the reference here anyway. Remember something strange happens here, with the script running twice or the like and not picking up the session state fully or vice versa, so sometimes we see strange behavior as a result though it's usually not a bit deal. In this case I've taken care of it anyway below by resetting the index on default_df_contents2
-    session_state.curr_url = st_javascript("await fetch('').then(r => window.parent.location.href)")
+    add_logo('app_images/mawa_logo-width315.png', height=150)
+
+    # Apply pages order and indentation
+    add_indentation()
+    show_pages_from_config()
+
+    # Sidebar organization
+    with st.sidebar:
+        st.write('**:book: [Documentation](https://ncats.github.io/multiplex-analysis-web-apps)**')
+        if newtab_flag:
+            # Get link for app to be opened in new tab and add link to docs
+            # This line is causing a repeatable "KeyError: 'platform'" that seems 
+            # not to have any ill effects. This is fixed by putting this line after the 
+            # "if 'platform' not in st.session_state:" block and "platform = st.session_state['platform']" line
+            url = st_javascript("await fetch('').then(r => window.parent.location.href)")  
+            st.write('*[Open app in new tab]({}) (may only work in Chrome or Edge, not Firefox)*'.format(url))
+
 
     # Check the platform
     session_state = check_for_platform(session_state)
