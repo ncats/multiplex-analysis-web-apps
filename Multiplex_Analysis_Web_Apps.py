7be6a3d29175e859a6815a1f8954e2140e937231
Dante Smith <djsmith@bu.edu>
2024-01-24 23:22:01 -0600

    Removing st_javascript and new tab requirements
    also some linting

--- Multiplex_Analysis_Web_Apps.py
+++ Multiplex_Analysis_Web_Apps.py
@@ -1,25 +1,39 @@
+'''
+Top level Streamlit Application for MAWA
+'''
+
 # Import relevant libraries
+import os
 import streamlit as st
-from streamlit_javascript import st_javascript
 
 # Import relevant libraries
 import nidap_dashboard_lib as ndl   # Useful functions for dashboards connected to NIDAP
 import app_top_of_page as top
 import streamlit_dataframe_editor as sde
 
-# Define the single, main function
 def main():
+    '''
+    Define the single, main function
+    '''
 
     # Set a wide layout
     st.set_page_config(layout="wide")
+
+    input_path = './input'
+    if not os.path.exists(input_path):
+        os.makedirs(input_path)
+
+    output_path = './output'
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
 
     # Run streamlit-dataframe-editor library initialization tasks at the top of the page
     st.session_state = sde.initialize_session_state(st.session_state)
 
     # Run Top of Page (TOP) functions
-    st.session_state = top.top_of_page_reqs(st.session_state, newtab_flag=True)
+    st.session_state = top.top_of_page_reqs(st.session_state)
 
-    # Markdown text 
+    # Markdown text
     intro_markdown = ndl.read_markdown_file('markdown/MAWA_WelcomePage.md')
     st.markdown(intro_markdown, unsafe_allow_html=True)
 
--- app_top_of_page.py
+++ app_top_of_page.py
@@ -1,12 +1,15 @@
+'''
+Set of methods that organize the functions and processes
+that must be run first at the top of every Streamlit page
+'''
+import subprocess
 import numpy as np
 import platform_io
-import subprocess
 import streamlit as st
 from st_pages import show_pages_from_config, add_indentation
-from streamlit_javascript import st_javascript
 from streamlit_extras.app_logo import add_logo
 
-def top_of_page_reqs(session_state, newtab_flag = False):
+def top_of_page_reqs(session_state):
     '''
     Top of the page requirements. These are the commands
     that are run at the top of the page that help maintain 
@@ -27,14 +30,6 @@ def top_of_page_reqs(session_state, newtab_flag = False):
     # Sidebar organization
     with st.sidebar:
         st.write('**:book: [Documentation](https://nidap.nih.gov/workspace/module/view/latest/ri.workshop.main.module.6b54d691-4d02-4f77-8acd-734f42e3eee0)**')
-        if newtab_flag:
-            # Get link for app to be opened in new tab and add link to docs
-            # This line is causing a repeatable "KeyError: 'platform'" that seems 
-            # not to have any ill effects. This is fixed by putting this line after the 
-            # "if 'platform' not in st.session_state:" block and "platform = st.session_state['platform']" line
-            url = st_javascript("await fetch('').then(r => window.parent.location.href)")  
-            st.write('*[Open app in new tab]({}) (may only work in Chrome or Edge, not Firefox)*'.format(url))
-
 
     # Check the platform
     session_state = check_for_platform(session_state)
@@ -42,9 +37,15 @@ def top_of_page_reqs(session_state, newtab_flag = False):
     return session_state
 
 def platform_is_nidap():
+    '''
+    Check if the Streamlit application is operating on NIDAP
+    '''
     return np.any(['nidap.nih.gov' in x for x in subprocess.run('conda config --show channels', shell=True, capture_output=True).stdout.decode().split('\n')[1:-1]])
 
 def check_for_platform(session_state):
+    '''
+    Set the platform parameters based on the platform the Streamlit app is running on
+    '''
     # Initialize the platform object
     if 'platform' not in session_state:
         session_state['platform'] = platform_io.Platform(platform=('nidap' if platform_is_nidap() else 'local'))
