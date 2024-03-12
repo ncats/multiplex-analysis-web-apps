c882c1372773e07f156b40cac114ca8c5e4a3ecb
Andrew Weisman <andrew.weisman@nih.gov>
2024-03-12 15:30:03 -0400

    Add skeleton for automated phenotyping

---
+++ pages/automated_phenotyping.py
@@ -0,0 +1,28 @@
+# Import relevant libraries
+import streamlit as st
+import app_top_of_page as top
+import streamlit_dataframe_editor as sde
+
+def main():
+    """
+    Main function for the Automated Phenotyper page.
+    """
+
+    # Set page settings
+    st.set_page_config(layout='wide', page_title='Automated Phenotyper')
+    st.title('Automated Phenotyper')
+
+    # Run streamlit-dataframe-editor library initialization tasks at the top of the page
+    st.session_state = sde.initialize_session_state(st.session_state)
+
+    # Run Top of Page (TOP) functions
+    st.session_state = top.top_of_page_reqs(st.session_state)
+
+    st.write('insert your app here')
+
+    # Run streamlit-dataframe-editor library finalization tasks at the bottom of the page
+    st.session_state = sde.finalize_session_state(st.session_state)
+
+# Run the main function
+if __name__ == '__main__':
+    main()
