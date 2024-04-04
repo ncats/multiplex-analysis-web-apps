b068030ce825dbdec197241236c778d6fe645c8f
abombin <abombin93@gmail.com>
2024-04-03 20:04:47 -0400

    formating wilcox data table

--- Drafts/Wilcox.py
+++ Drafts/Wilcox.py
@@ -6,6 +6,7 @@ adata = sc.read("input/clust_dat.h5ad")
 
 def find_markers(adata, umap_cur_col):
     sc.tl.rank_genes_groups(adata, umap_cur_col, method="wilcoxon", layer="counts")
+    return(adata)
     
 
 find_markers(adata, "Cluster")
@@ -26,3 +27,12 @@ genePvals = pd.DataFrame(adata.uns["rank_genes_groups"]["pvals"])
 
 len(pd.unique(adata.obs["Cluster"]))
 len(geneNames.columns)
+
+def format_de_results(adata):
+    result = adata.uns["rank_genes_groups"]
+    gene_names = pd.DataFrame(adata.uns["rank_genes_groups"]["names"])
+    gene_pvals = pd.DataFrame(adata.uns["rank_genes_groups"]["pvals"])
+    for i in range(len(gene_names.columns)):
+        curCluster = gene_names.columns[i]
+        gene_names.iloc[:, i] = gene_names.iloc[:, i] + "_" + gene_pvals.iloc[:, i].astype(str)
+    
