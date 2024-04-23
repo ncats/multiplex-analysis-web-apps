3b5ef6c17c7718f9ac1f69d1582c6015e63c09ec
abombin <abombin93@gmail.com>
2024-04-23 05:03:27 -0400

    Add umaps and spatial plots to differential intensity page

--- pages/05b_Pheno_Cluster.py
+++ pages/05b_Pheno_Cluster.py
@@ -22,6 +22,7 @@ import basic_phenotyper_lib as bpl
 import nidap_dashboard_lib as ndl 
 
 
+
 # Functions 
 
 # clusters differential expression
@@ -87,7 +88,90 @@ def data_editor_change_callback():
     st.session_state.df_filt = ndl.perform_filtering(st.session_state)
 
     # Set Figure Objects based on updated df
-    st.session_state = ndl.setFigureObjs(st.session_state, st.session_state.pointstSliderVal_Sel)        
+    st.session_state = ndl.setFigureObjs(st.session_state, st.session_state.pointstSliderVal_Sel)  
+          
+def phenocluster__plotly_umaps_b(adata, umap_cur_col, umap_cur_groups, umap_color_col, plot_column):
+    with plot_column:
+        subcol1, subcol2 = st.columns(2)
+        for i, umap_cur_group in enumerate(umap_cur_groups):
+            if umap_cur_group == "All":
+                subDat = adata
+            else:
+                subDat = adata[adata.obs[umap_cur_col] == umap_cur_group]
+            umap_coords = subDat.obsm['X_umap']
+            df = pd.DataFrame(umap_coords, columns=['UMAP_1', 'UMAP_2'])
+            clustersList = list(subDat.obs[umap_color_col] )
+            df[umap_color_col] = clustersList
+            df[umap_color_col] = df[umap_color_col].astype(str)
+            # Create the seaborn plot
+            fig = px.scatter(df, 
+             x="UMAP_1", 
+             y="UMAP_2", 
+             color=umap_color_col, 
+             title="UMAP " + umap_cur_group
+             #color_discrete_sequence=px.colors.sequential.Plasma
+             )
+            fig.update_traces(marker=dict(size=3)) # Adjust the size of the dots
+            fig.update_layout(
+                title=dict(
+                    text="UMAP " + umap_cur_group,
+                    x=0.5, # Center the title
+                    xanchor='center',
+                    yanchor='top'
+                ),
+                legend=dict(
+                    orientation="h",
+                    yanchor="top",
+                    y=-0.2,
+                    xanchor="right",
+                    x=1
+                )
+            )
+            if i % 2 == 0:
+                subcol1.plotly_chart(fig, use_container_width=True)
+            else:
+                subcol2.plotly_chart(fig, use_container_width=True)
+
+def spatial_plots_cust_2b(adata, umap_cur_col, umap_cur_groups, umap_color_col, plot_column):
+    with plot_column:
+        subcol3, subcol4 = st.columns(2)
+        for i, umap_cur_group in enumerate(umap_cur_groups):
+            if umap_cur_group == "All":
+                subDat = adata
+            else:
+                subDat = adata[adata.obs[umap_cur_col] == umap_cur_group]
+            umap_coords = subDat.obs[['Centroid X (µm)_(standardized)', 'Centroid Y (µm)_(standardized)']]
+            df = pd.DataFrame(umap_coords).reset_index().drop('index', axis = 1)
+            clustersList = list(subDat.obs[umap_color_col] )
+            df[umap_color_col] = clustersList
+            df[umap_color_col] = df[umap_color_col].astype(str)
+            fig = px.scatter(df, 
+             x="Centroid X (µm)_(standardized)", 
+             y="Centroid Y (µm)_(standardized)", 
+             color=umap_color_col, 
+             title="Spatial " + umap_cur_group
+             #color_discrete_sequence=px.colors.sequential.Plasma
+             )
+            fig.update_traces(marker=dict(size=3)) # Adjust the size of the dots
+            fig.update_layout(
+                title=dict(
+                    text="Spatial " + umap_cur_group,
+                    x=0.5, # Center the title
+                    xanchor='center',
+                    yanchor='top'
+                ),
+                legend=dict(
+                    orientation="h",
+                    yanchor="top",
+                    y=-0.2,
+                    xanchor="right",
+                    x=1
+                )
+            )
+            if i % 2 == 0:
+                subcol3.plotly_chart(fig, use_container_width=True)
+            else:
+                subcol4.plotly_chart(fig, use_container_width=True)
 
 
 def main():
@@ -138,13 +222,51 @@ def main():
             
         #st.session_state['phenocluster__edit_names_result_2'].dataframe_editor(on_change=data_editor_change_callback, reset_data_editor_button_text='Reset New Clusters Names')
         st.session_state['phenocluster__edit_names_result_2'].dataframe_editor(reset_data_editor_button_text='Reset New Clusters Names')
-        edit_clustering_names_2 = st.session_state['phenocluster__edit_names_result_2']
+        
     with phenocluster__col5b:
          #Edit cluster names
         st.button('Edit Clusters Names', on_click=phenocluster__edit_cluster_names_2, args = [st.session_state['phenocluster__clustering_adata'], 
                                                                                             st.session_state['phenocluster__edit_names_result_2']
                                                                                             ])
-    
+    phenocluster__col7b, phenocluster__col8b, phenocluster__col9b = st.columns([1, 3, 3])
+    def make_all_plots_2():
+    # make umaps
+        phenocluster__plotly_umaps_b(st.session_state['phenocluster__clustering_adata'], 
+            st.session_state['phenocluster__umap_cur_col'], 
+            st.session_state['phenocluster__umap_cur_groups'],
+            st.session_state['phenocluster__umap_color_col'],
+            phenocluster__col8b
+            )
+    # make spatial plots
+        spatial_plots_cust_2b(st.session_state['phenocluster__clustering_adata'], 
+            st.session_state['phenocluster__umap_cur_col'], 
+            st.session_state['phenocluster__umap_cur_groups'],
+            st.session_state['phenocluster__umap_color_col'],
+            phenocluster__col9b
+            )
+    with phenocluster__col7b:
+            # select column for umap coloring
+            st.session_state['phenocluster__umeta_columns'] = list(st.session_state['phenocluster__clustering_adata'].obs.columns)
+            if 'Edit_Cluster' in st.session_state['phenocluster__umeta_columns']:
+                st.session_state['phenocluster__umap_color_col_index'] = st.session_state['phenocluster__umeta_columns'].index('Edit_Cluster')
+            else:
+                st.session_state['phenocluster__umap_color_col_index'] = st.session_state['phenocluster__umeta_columns'].index(st.session_state['phenocluster__umap_color_col'])
+                
+            st.session_state['phenocluster__umap_color_col'] = st.selectbox('Select column for UMAP coloring:', 
+                                                                    st.session_state['phenocluster__umeta_columns'],
+                                                                    index=st.session_state['phenocluster__umap_color_col_index']
+                                                                    )
+            # select column for umap subsetting
+            st.session_state['phenocluster__umap_cur_col'] = st.selectbox('Select column for UMAP subsetting:', 
+                                                                    st.session_state['phenocluster__umeta_columns'], key='phenocluster__umap_col_dropdown_subset'
+                                                                    )
+            # list of available subsetting options
+            umap_cur_groups=  ["All"] + list(pd.unique(st.session_state['phenocluster__clustering_adata'].obs[st.session_state['phenocluster__umap_cur_col']]))
+            umap_sel_groups = st.multiselect('Select groups to be plotted on UMAP',
+                                                                            options = umap_cur_groups)
+            st.session_state['phenocluster__umap_cur_groups'] = umap_sel_groups
+            
+            st.button('Make Plots' , on_click=make_all_plots_2)
         
 
 # Run the main function
