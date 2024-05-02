85a6b6fc1e9fb19fb189f912676275148f083d72
Andrew Weisman <andrew.weisman@nih.gov>
2024-05-02 15:18:03 -0400

    Make major updates to the memory analyzer in preparation for using its logic in saving of the Streamlit session states

--- pages/memory_analyzer.py
+++ pages/memory_analyzer.py
@@ -3,8 +3,10 @@ import streamlit as st
 import app_top_of_page as top
 import streamlit_dataframe_editor as sde
 import pandas as pd
-import streamlit_dataframe_editor
 from pympler import asizeof
+
+
+bytes_to_mb = 1024 ** 2
 
 
 def get_object_class(value):
@@ -49,36 +51,59 @@ def get_object_class(value):
     return class_str
 
 
-def assess_whether_same_object(df):
-    # Note that if everything is commented out except for `return df`, the same number of session state keys exists, but instead all are "keys that would actually be saved" and there are none that "would not actually be saved." This is true for calls below like `df_do_not_save = assess_whether_same_object(df_do_not_save)` and `df_do_save = assess_whether_same_object(df_do_save)`. Make no sense to me. Same goes if I merely instead say `df_do_not_save = df_do_not_save` and `df_do_save = df_do_save`.
-    df = df.reset_index(drop=True)
-    df['is_same_as_above'] = None
-    for i in range(1, len(df)):
-        if df.loc[i, 'size_mb'] < 1:
-            df.loc[i, 'is_same_as_above'] = None
-        else:
-            current_key = df.loc[i, 'key']
-            previous_key = df.loc[i-1, 'key']
-            current_data = st.session_state.get(current_key, 'current_data')
-            previous_data = st.session_state.get(previous_key, 'previous_data')
-            current_data_attributes = getattr(current_data, 'data', 'current_data_attributes')
-            previous_data_attributes = getattr(previous_data, 'data', 'previous_data_attributes')
-            if (current_data is previous_data) or (current_data is previous_data_attributes) or (previous_data is current_data_attributes) or (current_data_attributes is previous_data_attributes):
-                df.loc[i, 'is_same_as_above'] = True
-            else:
-                df.loc[i, 'is_same_as_above'] = False
-    return df
+def split_off_dataset_formats_data_attribute(memory_usage_in_mb, saved_streamlit_session_state_key='session_selection'):
+
+    # For every item in the session state...
+    for key, value in st.session_state.items():
+
+        # If we don't want to save the key (__do_not_persist and FormSubmitter keys) or if we're looking at the saved_streamlit_session_state_key, then skip
+        if (not key.endswith('__do_not_persist')) and (not key.startswith('FormSubmitter:')) and (key != saved_streamlit_session_state_key):
+
+            # Get the type and size of the current object
+            # type1 = type(st.session_state[key])
+            type2 = get_object_class(value)
+            # predicted_size_in_mb = asizeof.asizeof(value) / bytes_to_mb  # note this is slow
+            predicted_size_in_mb = memory_usage_in_mb[key]
+
+            # If in st.session_state, are large, and are custom, we have problems, see overall comment above
+            if (predicted_size_in_mb > 1) and (type2 == 'dataset_formats.Standardized'):
+
+                # Write what we're doing
+                st.write(f'Key {key} in the session state has a value that is large in size (> 1 MB) and is of custom format {type2}. Storing the "data" attribute separately...')
+
+                # Store the data attribute (which is a pandas.DataFrame) separately in the session state
+                st.session_state[key + '_dataset_formats_data_attribute'] = value.data
+
+                # Delete the data attribute from the current object
+                del st.session_state[key].data
 
 
-def analyze_memory_usage(saved_streamlit_session_state_key='session_selection'):
+def recombine_data_attribute_with_dataset_formats_object():
 
-    # This function is largely copied from streamlit_session_state_management.save_session_state()
-    
+    # For every item in the session state...
+    for key, value in st.session_state.items():
+
+        # If the key is a separately-saved data attribute, then recombine it with the corresponding object
+        if key.endswith('_dataset_formats_data_attribute'):
+
+            # Get the key of the object that we want to recombine the data attribute with
+            key_of_object = key.replace('_dataset_formats_data_attribute', '')
+
+            # Make sure the key of the object exists in the session state
+            assert key_of_object in st.session_state, f'Key {key_of_object} does not exist in the session state, so we cannot recombine the data attribute {key} with its corresponding object.'
+
+            # Recombine the data attribute with the object
+            st.session_state[key_of_object].data = value
+
+            # Delete the separately-saved data attribute
+            del st.session_state[key]
+
+
+def write_dataframe_info_and_get_memory_usage(saved_streamlit_session_state_key='session_selection'):
     key_holder = []
     type_holder1 = []
     type_holder2 = []
     size_holder = []
-    bytes_to_mb = 1024 ** 2
     for key, value in st.session_state.items():
         if (not key.endswith('__do_not_persist')) and (not key.startswith('FormSubmitter:')) and (key != saved_streamlit_session_state_key):
             type1 = type(st.session_state[key])
@@ -88,87 +113,33 @@ def analyze_memory_usage(saved_streamlit_session_state_key='session_selection'):
             type_holder1.append(type1)
             type_holder2.append(type2)
             size_holder.append(predicted_size_in_mb)
+    df_to_write = pd.DataFrame({'key': key_holder, 'type1': type_holder1, 'type2': type_holder2, 'size_mb': size_holder}).sort_values(by='size_mb', ascending=False)
+    ser_is_same_as_above = assess_whether_same_object(df_to_write)
+    ser_pickle_or_dill = df_to_write['size_mb'].apply(lambda x: 'pickle' if x > 1 else 'dill')
+    ser_pickle_or_dill.name = 'pickle_or_dill'
+    df_to_write = pd.concat([df_to_write, ser_is_same_as_above, ser_pickle_or_dill], axis='columns')
+    assert len(df_to_write) == len(ser_is_same_as_above) == len(ser_pickle_or_dill), f'Lengths of dataframes (df_to_write: {len(df_to_write)}, ser_is_same_as_above: {len(ser_is_same_as_above)}, ser_pickle_or_dill: {len(ser_pickle_or_dill)}) do not match.'
+    st.dataframe(df_to_write)
+    return df_to_write.set_index('key')['size_mb']
 
-            # If in st.session_state, are large, and are custom, we have problems
-            if (predicted_size_in_mb > 1) and (type2 == 'dataset_formats.Standardized'):
-                st.write(f'Key {key} in the session state has a value that is large in size (> 1 MB) and is of custom format {type2}')
-                # WRITE CODE HERE THAT WILL STORE THE DATA ATTRIBUTE SEPARATELY IN THE SESSION STATE AND DELETE IT FROM THE CURRENT OBJECT
-                # THEN, rewrite dataframe showing size of each object to confirm the following will be correct
-                # Save all eligible objects in the session state that are large (which should now be all native) using pickle, and save the rest of the eligible objects in the session state using dill
-                # When loading in the resulting pickle and dill files, check for existence of the separately-saved data attribute and re-combine it with the corresponding object
 
-    st.dataframe(pd.DataFrame({'key': key_holder, 'type1': type_holder1, 'type2': type_holder2, 'size_mb': size_holder}))
-
-    st.write(f'Size of session state: {len(st.session_state)}')
-
-    # Create a dictionary of most items in the session state
-    session_dict = {}
-    keys_to_exclude = []
-    for key, value in st.session_state.items():
-        if (not key.endswith('__do_not_persist')) and (not key.startswith('FormSubmitter:')) and (key != saved_streamlit_session_state_key):
-            if isinstance(value, (sde.DataframeEditor, streamlit_dataframe_editor.DataframeEditor)):  # if this still doesn't seem to catch all DataframeEditor objects, try converting the type to a string and then checking if it contains 'DataframeEditor' or something like that
-                print(f'Analyzing components of dataframe editor {key}')
-                dataframe_editor_components = {
-                    'df_name': value.df_name,
-                    'default_df_contents': value.default_df_contents,
-                    'edited_dataframe': value.reconstruct_edited_dataframe()
-                }
-                dataframe_editor_components_name = 'dataframe_editor_components__' + key
-                session_dict[dataframe_editor_components_name] = dataframe_editor_components
-                keys_to_exclude.append(value.df_name)
-                keys_to_exclude.append(value.df_name + '_changes_dict')
-                keys_to_exclude.append(value.df_name + '_key')
+def assess_whether_same_object(df):
+    # Note that if everything is commented out except for `return df`, the same number of session state keys exists, but instead all are "keys that would actually be saved" and there are none that "would not actually be saved." This is true for calls below like `df_do_not_save = assess_whether_same_object(df_do_not_save)` and `df_do_save = assess_whether_same_object(df_do_save)`. Makse no sense to me. Same goes if I merely instead say `df_do_not_save = df_do_not_save` and `df_do_save = df_do_save`.
+    # Maybe this won't happen anymore since I'm just creating a separate series so I'm not modifying df in any way
+    ser_is_same_as_above = pd.Series(None, name='is_same_as_above', index=df.index)
+    for i in range(1, len(df)):
+        if df.iloc[i]['size_mb'] > 1:
+            current_key = df.iloc[i]['key']
+            previous_key = df.iloc[i-1]['key']
+            current_data = st.session_state.get(current_key, 'current_data')
+            previous_data = st.session_state.get(previous_key, 'previous_data')
+            current_data_attributes = getattr(current_data, 'data', 'current_data_attributes')
+            previous_data_attributes = getattr(previous_data, 'data', 'previous_data_attributes')
+            if (current_data is previous_data) or (current_data is previous_data_attributes) or (previous_data is current_data_attributes) or (current_data_attributes is previous_data_attributes):
+                ser_is_same_as_above.iloc[i] = True
             else:
-                print(f'Would save {key} of type {type(value)}')
-                session_dict[key] = value
-
-    # For each key to exclude, delete it from session_dict, i.e., the thing being saved to disk. This should allow a new key to be assigned to the st.data_editor() object within any dataframe editor objects that are later initialized in the load_session_state() function. Otherwise, the key will always be the same so upon loading a session state, the st.data_editor() object will not be redrawn. The solution to that is to force the st.data_editor() object to be redrawn by forcing its key to change.
-    bytes_to_mb = 1024 ** 2
-    tot_size_in_memory_do_not_save = 0
-    keys_holder_do_not_save = []
-    size_holder_do_not_save = []
-    for key in keys_to_exclude:
-        if key in session_dict:
-            value = session_dict[key]
-            predicted_size_in_mb = asizeof.asizeof(value) / bytes_to_mb
-            print(f'Wouldn\'t actually save {key} of type {type(value)}, which would take around {predicted_size_in_mb:.2f} MB...')
-            tot_size_in_memory_do_not_save += predicted_size_in_mb
-            keys_holder_do_not_save.append(key)
-            size_holder_do_not_save.append(predicted_size_in_mb)
-            del session_dict[key]
-
-    # Print the total size in memory
-    print(f'Total size in memory (i.e., predicted) of keys that would not actually be saved: {tot_size_in_memory_do_not_save:.2f} MB')
-
-    # Save the dictionary to the pickle file. Note this no longer randomly crashes with "PicklingError: Can't pickle <class 'streamlit_dataframe_editor.DataframeEditor'>: it's not the same object as streamlit_dataframe_editor.DataframeEditor" because we're no longer saving the DataframeEditor object itself, but rather the initialization data and the current contents. Note that using dill did also solve the problem, which if we were to use dill, we could try saving the entire session at once (instead of individual objects) and also thereby include difficult items such as st.form objects. NOTE: If we start getting the error again, try either using dill or probably better yet, excluding other custom object types from being saved in the first place, e.g., class 'platform_io.Platform'. Such exclusion would be done in keys_to_exclude as above.
-    tot_size_in_memory_mb_do_save = 0
-    keys_holder_do_save = []
-    size_holder_do_save = []
-    for key, value in session_dict.items():
-        predicted_size_in_mb = asizeof.asizeof(value) / bytes_to_mb
-        print(f'Would save {key} of type {type(value)} to a pickle file, which would take around {predicted_size_in_mb:.2f} MB...')
-        tot_size_in_memory_mb_do_save += predicted_size_in_mb
-        keys_holder_do_save.append(key)
-        size_holder_do_save.append(predicted_size_in_mb)
-
-    # Print the total size in memory
-    print(f'Total size in memory (i.e., predicted) of keys that would indeed be saved: {tot_size_in_memory_mb_do_save:.2f} MB')
-
-    # Write a dataframe of the keys and sizes that will not be saved, sorted in descending order of size
-    df_do_not_save = pd.DataFrame({'key': keys_holder_do_not_save, 'size_mb': size_holder_do_not_save})
-    df_do_not_save = df_do_not_save.sort_values(by='size_mb', ascending=False)
-    # df_do_not_save = df_do_not_save.copy()  # this does different stuff than df_do_not_save = df_do_not_save a bit strangely
-    df_do_not_save = assess_whether_same_object(df_do_not_save.copy())
-    st.write(f'Keys that would not actually be saved (total {tot_size_in_memory_do_not_save:.2f} MB):')
-    st.dataframe(df_do_not_save)
-
-    # Write a dataframe of the keys and sizes that will be saved, sorted in descending order of size
-    df_do_save = pd.DataFrame({'key': keys_holder_do_save, 'size_mb': size_holder_do_save})
-    df_do_save = df_do_save.sort_values(by='size_mb', ascending=False)
-    # df_do_save = df_do_save.copy()  # this does different stuff than df_do_save = df_do_save a bit strangely
-    df_do_save = assess_whether_same_object(df_do_save.copy())
-    st.write(f'Keys that would actually be saved (total {tot_size_in_memory_mb_do_save:.2f} MB):')
-    st.dataframe(df_do_save)
+                ser_is_same_as_above.iloc[i] = False
+    return ser_is_same_as_above
 
 
 def main():
@@ -176,7 +147,28 @@ def main():
     Main function for the page.
     """
 
-    analyze_memory_usage()
+    # The idea is that pickle cannot generally pickle objects from custom classes so we want to use dill for that. However, dill is much slower for large data structures such as large pandas dataframes, which are supported by pickle. Therefore, we want to use pickle for large "native" objects and use dill for everything else. Therefore we need to know both the type and size of each object and save the small ones using dill and save the large ones using pickle. However, sometimes there are large objects that are of custom classes, which we can't save using pickle. Therefore, we need to save the data attribute of such objects separately and then delete the data attribute from the object before saving the object using pickle. Upon loading the object, we need to check for the existence of the separately-saved data attribute and re-combine it with the object.
+
+    # Define the key of the session state object that we want to save
+    saved_streamlit_session_state_key = 'session_selection'
+
+    # Write the session state object information to screen
+    st.write('Initial session state object information:')
+    memory_usage_in_mb = write_dataframe_info_and_get_memory_usage(saved_streamlit_session_state_key=saved_streamlit_session_state_key)
+
+    # Split off the data attribute from the dataset_formats objects
+    split_off_dataset_formats_data_attribute(memory_usage_in_mb, saved_streamlit_session_state_key=saved_streamlit_session_state_key)
+
+    # Write the session state object information to screen
+    st.write('Session state object information after splitting off data attributes from dataset_formats objects:')
+    memory_usage_in_mb = write_dataframe_info_and_get_memory_usage(saved_streamlit_session_state_key=saved_streamlit_session_state_key)
+
+    # Recombine the data attribute with the dataset_formats objects
+    recombine_data_attribute_with_dataset_formats_object()
+
+    # Write the session state object information to screen
+    st.write('Session state object information after recombining data attributes with dataset_formats objects:')
+    memory_usage_in_mb = write_dataframe_info_and_get_memory_usage(saved_streamlit_session_state_key=saved_streamlit_session_state_key)
 
 
 # Run the main function
