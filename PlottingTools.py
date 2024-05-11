71aa7b7392c3ad1c1bff0aea4dc03e7275088c56
Dante Smith <djsmith@bu.edu>
2024-05-10 22:58:04 -0500

    Multiplying the density matrix by 100
    Going to be plotting by percentage instead of decimal

--- PlottingTools.py
+++ PlottingTools.py
@@ -54,6 +54,7 @@ def plot_2d_density(X, Y=None, bins=200, n_pad=40, w=None, ax=None, gaussian_sig
         else:
             d, xedges, yedges = np.histogram2d(X, Y, bins=bins)
             d /= np.sum(d)
+            d = d*100
             d = ndi.gaussian_filter(d.T, sigma=gaussian_sigma)
     else:
         d = X
