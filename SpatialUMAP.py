7e07960cf5c5d0fc6d43a99911625f4ac1a6996f
Dante Smith <djsmith@bu.edu>
2024-05-20 18:39:50 -0500

    Removing a defunct function
    This function has been replaced by a new faster method in the Utils file

--- SpatialUMAP.py
+++ SpatialUMAP.py
@@ -206,9 +206,6 @@ class SpatialUMAP:
 
         # Create a pool of worker processes
         with mp.Pool(processes=num_cpus_to_use) as pool:
-
-            # Apply the calculate_density_matrix_for_image function to each set of keyword arguments in kwargs_list
-            # A single call would be something like: calculate_density_matrix_for_image(**kwargs_list[4])
             results = pool.starmap(utils.fast_neighbors_counts_for_block, kwargs_list)
 
         print(f'All images took {(time.time() - start_time) / 60:.2f} minutes to complete')
@@ -228,104 +225,6 @@ class SpatialUMAP:
 
         # Concatenate the results into a single dataframe
         return full_array
-
-    @staticmethod
-    def calculate_density_matrix_for_image(df_image, phenotypes, phenotype_column_name, image, coord_column_names, radii, range_strings, debug_output=False, swap_inequalities=False):
-        """
-        Calculate the density matrix for a single image.
-
-        Args:
-            df_image (pandas.DataFrame): The dataframe containing the data for the current image.
-            phenotypes (numpy.ndarray): The array of phenotypes.
-            phenotype_column_name (str): The name of the column containing the phenotype information.
-            image (str): The name of the current image.
-            coord_column_names (list): The list of column names containing the coordinate information.
-            radii (numpy.ndarray): The array of radii.
-            range_strings (list): The list of range strings.
-            debug_output (bool, optional): Whether to print debug output.
-
-        Returns:
-            pandas.DataFrame: The dataframe containing the density matrix for the current image.
-        """
-
-        # Initialize the start time
-        start_time = time.time()
-
-        # Get the number of range segments
-        num_ranges = len(radii) - 1
-
-        # Initialize the dataframe to store the number of neighbors for the current image
-        df_num_neighbors_image = pd.DataFrame(index=df_image.index)
-
-        # Loop through the phenotypes as center phenotypes
-        for center_phenotype in phenotypes:
-
-            # Get the locations of the current image and center phenotype in the dataframe
-            center_loc_for_image = df_image[phenotype_column_name] == center_phenotype
-
-            # Get the total number of centers of the current type in the current image
-            num_centers_in_image = center_loc_for_image.sum()
-
-            # If there are no centers of the current type in the current image, print a message
-            if num_centers_in_image == 0:
-                if debug_output:
-                    pass
-                    # print(f'No centers found for image {image} and phenotype {center_phenotype}')
-
-            # Otherwise, calculate the number of neighbors of each type in the current image, for all neighbor phenotypes and all radii
-            else:
-
-                # Get the coordinates of the centers of the current type in the current image as a numpy array
-                arr_image_center_phenotype = df_image[center_loc_for_image][coord_column_names].to_numpy()
-
-                # Loop through the phenotypes as neighbor phenotypes
-                for neighbor_phenotype in phenotypes:
-
-                    # Get the locations of the current image and neighbor phenotype in the dataframe
-                    neighbor_loc_for_image = df_image[phenotype_column_name] == neighbor_phenotype
-
-                    # Get the total number of neighbors of the current type in the current image
-                    num_neighbors_in_image = neighbor_loc_for_image.sum()
-
-                    # If there are no neighbors of the current type in the current image, print a message
-                    if num_neighbors_in_image == 0:
-                        if debug_output:
-                            pass
-                            # print(f'No neighbors found for image {image} and phenotype {neighbor_phenotype}')
-
-                    # Otherwise, calculate the number of neighbors of the current type in the current image, for all radii
-                    else:
-
-                        # Print the number of centers and neighbors found for the current image and phenotypes
-                        if debug_output:
-                            pass
-                            # print(f'Number of centers found for image {image} and phenotype {center_phenotype}: {num_centers_in_image}')
-                            # print(f'Number of neighbors found for image {image} and phenotype {neighbor_phenotype}: {num_neighbors_in_image}')
-
-                        # Get the coordinates of the neighbors of the current type in the current image as a numpy array
-                        arr_image_neighbor_phenotype = df_image[neighbor_loc_for_image][coord_column_names].to_numpy()
-
-                        # Calculate the number of neighbors around the centers of the current types in the current image, in each radii range
-                        nneighbors = utils.calculate_neighbor_counts_with_possible_chunking(center_coords = arr_image_center_phenotype,
-                                                                                            neighbor_coords = arr_image_neighbor_phenotype,
-                                                                                            radii = radii,
-                                                                                            single_dist_mat_cutoff_in_mb = 200,
-                                                                                            test = False,
-                                                                                            verbose = False,
-                                                                                            swap_inequalities = swap_inequalities)  # (num_centers, num_ranges)
-
-                        # Add the number of neighbors to the dataframe
-                        for irange in range(num_ranges):
-                            range_string = range_strings[irange]
-                            # note that since we are adding columns dynamically that the order of these columns may not be logical because sometimes there are no centers or no neighbors
-                            df_num_neighbors_image.loc[center_loc_for_image, f'{neighbor_phenotype} in range {range_string}'] = nneighbors[:, irange]
-
-        # Print the time taken to calculate the number of neighbors for the current image
-        if debug_output:
-            print(f'Time to calculate neighbors for image {image} ({len(df_image)} rows) on a single CPU: {(time.time() - start_time) / 60:.2f} minutes')
-
-        # Return the dataframe with the number of neighbors for the current image
-        return df_num_neighbors_image
 
     def __init__(self, dist_bin_um, um_per_px, area_downsample):
         # microns per pixel
@@ -510,7 +409,6 @@ class SpatialUMAP:
         '''
         Andrew's method for getting counts
         '''
-        print('Performing Counts using Andrews Method')
         self.counts = self.calculate_density_matrix_for_all_images(debug_output=False, swap_inequalities=True)
 
     def get_areas(self, area_threshold, pool_size=2, save_file=None, plots_directory=None):
