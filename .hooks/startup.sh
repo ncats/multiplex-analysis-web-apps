d0c949322387fcb49950e509b4e94b442f5254cf
Andrei Bombin <abombin93@gmail.com>
2024-06-03 05:51:58 -0400

    updated startup.sh to correspond to develop branch

--- .hooks/startup.sh
+++ .hooks/startup.sh
@@ -8,5 +8,6 @@ set -e
 # Create custom conda environment with ipykernel so it can be used from Jupyter
 # mamba create -n my_env -y -q python==3.8 ipykernel foundry-transforms-lib-python
 
-#mamba install -y gcc_linux-64 gxx_linux-64
+pip install scikit-learn streamlit-extras squidpy split-file-reader st-pages dill pympler objsize
+mamba install -y -q natsort "foundry-transforms-lib-python>=0.578.0"
 mamba install -y gcc_linux-64 gxx_linux-64 && mamba install -y hnswlib
