01dee0d4689228888d60cc2414c036467d7ea7d3
Dante Smith <djsmith@bu.edu>
2024-06-18 01:02:58 -0400

    Making an attempt to fix the Stopping error

--- basic_phenotyper_lib.py
+++ basic_phenotyper_lib.py
@@ -700,8 +700,10 @@ def umap_clustering(spatial_umap, n_clusters, clust_minmax, cpu_pool_size = 8):
             )
         )
 
+    mp_start_method = mp.get_start_method()
+    # mp_start_method = 'forkserver'
     # Create a pool of worker processes
-    with mp.Pool(processes=cpu_pool_size) as pool:
+    with mp.get_context(mp_start_method).Pool(processes=cpu_pool_size) as pool:
         results = pool.starmap(kmeans_calc, kwargs_list)
 
     wcss = [x.inertia_ for x in results]
