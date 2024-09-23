cd510d535b2fc630901e6f2ea58ff3df82290772
Andrew Weisman <andrew.weisman@nih.gov>
2024-09-23 14:47:02 -0400

    Switch back to my original forking test which is more representative of how parallel calls are made in MAWA. The slow imports still occur the same way, we probably just need to fix those scripts

--- pages2/forking_test.py
+++ pages2/forking_test.py
@@ -1,15 +1,16 @@
 import streamlit as st
+import utils
 import time
-import multiprocessing
-import os
 
-def workder(n):
-    print(f'Worker {n} started in process {os.getpid()}')
-    time.sleep(0.5)
-    print(f'Worker {n} finished in process {os.getpid()}')
+
+def sleep_task(tuple_of_args):
+    sleep_time_sec = tuple_of_args[0]
+    print(f'Single task running, waiting for {sleep_time_sec} seconds')
+    time.sleep(sleep_time_sec)
 
 
 def main():
+
     if 'nworkers' not in st.session_state:
         st.session_state['nworkers'] = 1
     st.number_input('Number of workers', min_value=1, max_value=10, key='nworkers')
@@ -18,19 +19,17 @@ def main():
         st.session_state['num_tasks'] = 10
     st.number_input('Number of tasks', min_value=1, max_value=100, key='num_tasks')
 
+    start_time = time.time()
     if st.button('Run forking test'):
-        start_time = time.time()
+
         st.write('Forking test button pressed')
-        
-        multiprocessing.set_start_method('forkserver', force=True)
-        with multiprocessing.Pool(st.session_state['nworkers']) as pool:
-            pool.map(workder, range(st.session_state['num_tasks']))
+
+        utils.execute_data_parallelism_potentially(sleep_task, [(1,)] * st.session_state['num_tasks'], nworkers=st.session_state['nworkers'], task_description='Sleeping tasks')
 
         st.write('Done')
 
-        st.write(f'Total time: {time.time() - start_time:.2f} seconds')
+    st.write(f'Total time: {time.time() - start_time:.2f} seconds')
 
 
 if __name__ == '__main__':
-    multiprocessing.set_start_method('forkserver', force=True)
     main()
