d4aa63b37d95e959a618ad7bad768b8e8935452c
Andrew Weisman <andrew.weisman@nih.gov>
2025-11-29 18:40:08 -0500

    Output the contents (i.e. keys if they're dicts) of the values upon serialziation.

--- source/framework/utils.py
+++ source/framework/utils.py
@@ -82,6 +82,8 @@ def serialize_dictionary_to_binary_files(dictionary, dict_name, directory, ignor
         for key, value in dictionary.items():
             if ignore_do_not_persist_flag or (not key.endswith("__do_not_persist")):
                 print(f"Serializing key (before deconstruction): {key}, type: {type(value).__name__}", flush=True)
+                if isinstance(value, dict):
+                    print(f"  Keys in object: {list(value.keys())}", flush=True)
                 if key == "LAZYFRAMES":
                     val_copy = {lf_key: {k: v for k, v in lf_dict.items() if k != "lf"}
                                 for lf_key, lf_dict in value.items()}
@@ -95,6 +97,8 @@ def serialize_dictionary_to_binary_files(dictionary, dict_name, directory, ignor
                 else:
                     val_copy = value
                 print(f"Serializing key (after deconstruction): {key}, type: {type(val_copy).__name__}", flush=True)
+                if isinstance(val_copy, dict):
+                    print(f"  Keys in object: {list(val_copy.keys())}", flush=True)
                 save_dict[key] = val_copy
                 saved_objects_types[key] = type(val_copy).__name__
 
