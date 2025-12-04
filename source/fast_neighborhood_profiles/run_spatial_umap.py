fb5e3f39ebea20c53b58f805280ad76ec2645f1a
Andrew Weisman <andrew.weisman@nih.gov>
2025-12-03 21:04:13 -0500

    Correctly filter the lazyframes dictionary.

--- source/fast_neighborhood_profiles/run_spatial_umap.py
+++ source/fast_neighborhood_profiles/run_spatial_umap.py
@@ -108,7 +108,8 @@ def main():
 
     # Assemble the inputs (less the polars dataframe, to be part of the job preprocessing) to the spatial UMAP analysis.
     # We need all lazyframes so we can rebuild what we need inside the job worker, and we will parse down from that, once we've regenerated what's needed, inside the spatial UMAP wrapper. Then we'll input only what we need (the single input lazyframe, plus parameters) into the core spatial UMAP function.
-    inputs = dict(LAZYFRAMES=st.session_state["LAZYFRAMES"][["unified_input_file", "marker_phenotyping"]], unique_labels=unique_labels, dist_bin_um_list=dist_bin_um_list, area_downsample=area_downsample, um_per_px=1, cpu_pool_size=cpu_pool_size, subdir="spatial_umap", counts_method="andrew", area_threshold=area_threshold, custom_areas=custom_areas, seed_for_train_test_split=seed_for_train_test_split, n=n, keep_images_with_too_little_data=keep_images_with_too_little_data, train_sample_frac=train_sample_frac, test_sample_frac=test_sample_frac, de_min_coords=de_min_coords, mp_start_method='forkserver')
+    lazyframes = {k: st.session_state["LAZYFRAMES"][k] for k in ["unified_input_file", "marker_phenotyping"] if k in st.session_state["LAZYFRAMES"]}
+    inputs = dict(LAZYFRAMES=lazyframes, unique_labels=unique_labels, dist_bin_um_list=dist_bin_um_list, area_downsample=area_downsample, um_per_px=1, cpu_pool_size=cpu_pool_size, subdir="spatial_umap", counts_method="andrew", area_threshold=area_threshold, custom_areas=custom_areas, seed_for_train_test_split=seed_for_train_test_split, n=n, keep_images_with_too_little_data=keep_images_with_too_little_data, train_sample_frac=train_sample_frac, test_sample_frac=test_sample_frac, de_min_coords=de_min_coords, mp_start_method='forkserver')
 
     # Allow the user to run the spatial UMAP analysis asynchronously.
     analysis_framework.job_submission(
